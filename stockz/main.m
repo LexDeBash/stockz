fc6498d61ffb89e135f55047587949e44efa7fb3
Alexey Efimov <lex.efimov@gmail.com>
2016-04-26 00:54:48 +0500

    Создание нового проекта

---
+++ stockz/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  stockz
+//
+//  Created by Alexey Efimov on 26.04.16.
+//  Copyright © 2016 Alexey Efimov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
